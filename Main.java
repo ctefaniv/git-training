8a87a4e7bbf169ab18435d5c61a4f02ad321f03b
VictoriaYosufiv <victoriashologon@gmail.com>
2018-04-14 12:00:02 +0300

    virsh

---
+++ Main.java
@@ -0,0 +1,12 @@
+    public class Main {
+
+        public static void main(String[] args) {
+
+            System.out.println("А нумо знову віршувать.");
+            System.out.println("Звичайне, нишком. Нумо знову,");
+            System.out.println("Поки новинка на основі,");
+            System.out.println("Старинку божу лицювать.");
+
+
+        }
+    }
